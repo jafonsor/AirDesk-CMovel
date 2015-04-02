c931113be9681d62c7ec8cf636194e9a4943aae9
João Rodrigues <jrodriguesnum1@hotmail.com>
2015-04-02 22:31:28 +0100

    undo the move of AirDesk.java

--- app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/AirDesk.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/AirDesk.java
@@ -1,18 +1,14 @@
 package pt.ulisboa.tecnico.cmov.g15.airdesk;
 
-import android.app.Application;
-
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-import pt.ulisboa.tecnico.cmov.g15.airdesk.domain.ForeignWorkspace;
-import pt.ulisboa.tecnico.cmov.g15.airdesk.domain.OwnerWorkspace;
-import pt.ulisboa.tecnico.cmov.g15.airdesk.domain.User;
 import pt.ulisboa.tecnico.cmov.g15.airdesk.domain.Workspace;
 import pt.ulisboa.tecnico.cmov.g15.airdesk.network.INetworkServiceClient;
 import pt.ulisboa.tecnico.cmov.g15.airdesk.network.INetworkServiceServer;
 import pt.ulisboa.tecnico.cmov.g15.airdesk.network.NetworkServiceClient;
 import pt.ulisboa.tecnico.cmov.g15.airdesk.network.NetworkServiceServer;
+
 
 /**
  * Created by MSC on 02/04/2015.
