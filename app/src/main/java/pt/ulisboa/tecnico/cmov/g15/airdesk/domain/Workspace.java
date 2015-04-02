cbffa67df4b78e07f46b91745be806dea5c9e8ff
Diogo Andrade <diogo@yubo.be>
2015-04-03 00:15:51 +0100

    Starting Workspace VisibilityEnum added

--- app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/domain/Workspace.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/domain/Workspace.java
@@ -1,7 +1,18 @@
 package pt.ulisboa.tecnico.cmov.g15.airdesk.domain;
 
+import java.util.List;
+
+import pt.ulisboa.tecnico.cmov.g15.airdesk.domain.enums.Visibility;
+
 /**
  * Created by MSC on 02/04/2015.
  */
 public class Workspace {
+    User onwer;
+    String name;
+    double quote;
+    Boolean mounted;
+    List<User> accessList;
+    Visibility visibility;
+    List<String> tags;
 }
---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/domain/enums/Visibility.java
@@ -0,0 +1,9 @@
+package pt.ulisboa.tecnico.cmov.g15.airdesk.domain.enums;
+
+/**
+ * Created by diogo on 02-04-2015.
+ */
+public enum Visibility {
+    PUBLIC,
+    PRIVATE
+}
