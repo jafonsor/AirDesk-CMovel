a163f039ceedf2deadef7e06f4d39ce029ee5e4d
João Rodrigues <jrodriguesnum1@hotmail.com>
2015-04-04 18:51:19 +0100

    start using airdesk facade on the view

--- app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/view/WorkspaceListActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/view/WorkspaceListActivity.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import pt.ulisboa.tecnico.cmov.g15.airdesk.AirDesk;
 import pt.ulisboa.tecnico.cmov.g15.airdesk.GlobalContext;
 import pt.ulisboa.tecnico.cmov.g15.airdesk.R;
 import pt.ulisboa.tecnico.cmov.g15.airdesk.domain.Workspace;
@@ -42,7 +43,8 @@ public class WorkspaceListActivity extends ActionBarActivity {
         ListView listview = (ListView) findViewById(R.id.owned_workspaces);
 
         GlobalContext appContext = (GlobalContext) getApplicationContext();
-        final List<Workspace> ownerWorkspaces = appContext.getAirDesk().getOwnerWorkspaces();
+        AirDesk airDesk = appContext.getAirDesk();
+        final List<Workspace> ownerWorkspaces = airDesk.getOwnerWorkspaces();
 
         final ArrayList<String> list = new ArrayList<String>();
         list.add("ficheiro1.txt");
