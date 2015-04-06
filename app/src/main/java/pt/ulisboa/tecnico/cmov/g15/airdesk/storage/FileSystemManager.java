fda1e2b245d6a14d7039eba73b6d33fce9d02547
Diogo Andrade <diogo@yubo.be>
2015-04-06 18:57:39 +0100

    Added some logic in FileSystemManager

--- app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/storage/FileSystemManager.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/storage/FileSystemManager.java
@@ -9,22 +9,30 @@ import java.io.File;
  */
 public class FileSystemManager {
     File sdcard = Environment.getExternalStorageDirectory();
+    String state = Environment.getExternalStorageState();
 
     public File createFile(String dirPath, String name) {
+        File f = null;
         if(name != null)
-            if(dirPath == null)
-                return new File(dirPath,name+".txt");
-            else //by default
-                return new File(sdcard+"AirDesk/",name+".txt");
-
-        return null;
+            if(dirPath != null && state.equals(Environment.MEDIA_MOUNTED)) { //by default
+                f = new File(sdcard + "/AirDesk/", name + ".txt");
+                if(!f.exists())
+                    return f;
+            } else {
+                f = new File(dirPath, name + ".txt");
+                if(!f.exists())
+                    return f;
+            }
+        return null; // Já existe um ficheiro com esse nome
     }
 
     public File getFile(String filePath) {
-        if(filePath != null)
-            return new File(filePath);
+        File file = new File(filePath);
 
-        return null;
+        if(filePath != null)
+            if(file.exists())
+                return file;
+        return null; // O file que procura não existe ou não foi introduzido nenhum file path
     }
 
     public boolean deleteFile(File file){
