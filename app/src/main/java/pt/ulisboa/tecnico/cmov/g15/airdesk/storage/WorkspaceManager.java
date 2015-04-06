63df27750da8842ffcff91e5c4fb4fc57c2f7c10
Diogo Andrade <diogo@yubo.be>
2015-04-06 22:35:37 +0100

    TG-31 #ready-for-test

--- app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/storage/WorkspaceManager.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/storage/WorkspaceManager.java
@@ -1,5 +1,8 @@
 package pt.ulisboa.tecnico.cmov.g15.airdesk.storage;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Environment;
 
 import java.io.File;
@@ -16,6 +19,7 @@ import pt.ulisboa.tecnico.cmov.g15.airdesk.domain.Workspace;
  */
 public class WorkspaceManager {
     File sdcard = Environment.getExternalStorageDirectory();
+    File internal = Environment.getDataDirectory();
     String state = Environment.getExternalStorageState();
 
     public Workspace createWorkspace(User appUser,User wsOwner, String name, double quota) {
@@ -30,15 +34,41 @@ public class WorkspaceManager {
             Note: This approach is useful to have different files with the
                   same name in different workspaces.
          */
+        File newDir = null;
         if(state.equals(Environment.MEDIA_MOUNTED)) { // If has memory card
-            File newDirectory = new File(sdcard + "/AirDesk/", name);
-            newDirectory.mkdir();
+            newDir = new File(sdcard + "/AirDesk/", name);
+        } else {
+            newDir = new File(internal + "/AirDesk/", name);
         }
+        newDir.mkdir();
 
         return ws;
     }
 
     public void deleteWorkspace(Workspace workspace) {
+        /*
+            If it is to delete all the files when a workspace is destroyed, uncomment the follow code.
+         */
 
+        /*
+        File ws = null;
+
+        if(state.equals(Environment.MEDIA_MOUNTED)) { // If has memory card
+            ws = new File(sdcard + "/AirDesk/", workspace.getName());
+            if(ws.isDirectory()) {
+                String[] children = dir.list();
+                for (int i = 0; i < children.length; i++) {
+                    new File(dir, children[i]).delete();
+                ws.delete();
+            }
+        } else {
+            ws = new File(internal + "/AirDesk/", workspace.getName());
+            if(ws.isDirectory()){
+                String[] children = dir.list();
+                for (int i = 0; i < children.length; i++) {
+                    new File(dir, children[i]).delete();
+                ws.delete();
+            }
+        }*/
     }
 }
