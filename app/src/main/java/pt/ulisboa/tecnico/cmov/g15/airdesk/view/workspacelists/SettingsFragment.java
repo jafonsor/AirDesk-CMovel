1536bcbd5bef61289452bde497b7c58ca03b810d
marcocunha <marco.soudocunha@gmail.com>
2015-04-09 18:29:17 +0100

    Button Delete User

--- app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/view/workspacelists/SettingsFragment.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/g15/airdesk/view/workspacelists/SettingsFragment.java
@@ -5,9 +5,10 @@ package pt.ulisboa.tecnico.cmov.g15.airdesk.view.workspacelists;
  */
 
 import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,17 +17,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.io.File;
 
 import pt.ulisboa.tecnico.cmov.g15.airdesk.AirDesk;
 import pt.ulisboa.tecnico.cmov.g15.airdesk.R;
+import pt.ulisboa.tecnico.cmov.g15.airdesk.storage.FileSystemManager;
 import pt.ulisboa.tecnico.cmov.g15.airdesk.view.LoginActivity;
 
-public class SettingsFragment extends Fragment implements View.OnClickListener {
+public class SettingsFragment extends Fragment {
 
 
     private Button mSaveButton;
+    private Button mDeleteUserButton;
     private EditText mEmailET;
     private EditText mNickNameET;
 
@@ -34,6 +36,7 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.settings_layout, container, false);
+        mDeleteUserButton = (Button) rootView.findViewById(R.id.settings_deleteUserBtn);
         mSaveButton = (Button) rootView.findViewById(R.id.saveSettingsBtn);
         mEmailET = (EditText) rootView.findViewById(R.id.settingsEmailET);
         mNickNameET = (EditText) rootView.findViewById(R.id.settingsNicknameET);
@@ -41,12 +44,34 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
         AirDesk airDesk = (AirDesk) getActivity().getApplication();
         mEmailET.setText(airDesk.getUser().getEmail());
         mNickNameET.setText(airDesk.getUser().getUserName());
-        mSaveButton.setOnClickListener(this);
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickSaveButton(v);
+            }
+        });
+        mDeleteUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickDeleteUserButton(v);
+            }
+        });
         return rootView;
     }
 
-    @Override
-    public void onClick(View v) {
+    public void onClickDeleteUserButton(View v) {
+        SharedPreferences preferences = this.getActivity().getSharedPreferences(LoginActivity.SHARED_PREFS_FILE, Context.MODE_PRIVATE);
+        preferences.edit().clear().commit();
+
+        AirDesk airDesk = (AirDesk) getActivity().getApplication();
+
+        FileSystemManager.deleteRecursively(new File(Environment.getExternalStorageDirectory() + "/AirDesk/" + airDesk.getUser().getEmail()));
+        Toast.makeText(getActivity().getApplicationContext(), "User deleted", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this.getActivity(), LoginActivity.class));
+        this.getActivity().finish();
+    }
+
+    public void onClickSaveButton(View v) {
         //TODO verificar que email é unico
         AirDesk airDesk = (AirDesk) getActivity().getApplication();
         airDesk.getUser().setEmail(mEmailET.getText().toString().trim());
